399c224841d41846d2fcf4631b2ae20788157e52
medinah2 <33753845+medinah2@users.noreply.github.com>
2020-05-01 09:54:18 -0400

    Update welcome_controller.rb

--- src/app/controllers/welcome_controller.rb
+++ src/app/controllers/welcome_controller.rb
@@ -1,3 +1,7 @@
+# Creeport version 4 - 5.1.2020
+# auto generholds methods and variables that can be called by the welcome controller
+# License  GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007
+
 class WelcomeController < ApplicationController
   def index
   end
